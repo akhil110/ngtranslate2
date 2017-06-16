7e2bad57bd1b69a30aee08a2dc6d8348f85dbb5d
Akhilesh Srivastava <akhil110@gmail.com>
2017-06-16 13:54:52 -0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { TranslateModule, MissingTranslationHandler } from 'ng2-translate';
+import { TranslateModule, TranslateLoader, TranslateStaticLoader, MissingTranslationHandler } from 'ng2-translate';
 
 import { FormsModule } from "@angular/forms";
 
@@ -10,32 +10,40 @@ import { AppComponent }   from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { MyMissingTranslationHandler } from './missingtemplate.component'
+import { MyMissingTranslationHandler } from './missingtemplate.component';
+
+export function createTranslateLoader(http: Http) {
+  return new TranslateStaticLoader(http, './assets/i18n', '.json');
+}
 
 @NgModule({
   imports: [
-        BrowserModule,
-        HttpModule,
-        FormsModule,
-        RouterModule.forRoot([
-            { path: 'home', component: HomeComponent},
-            { path: 'about', component: AboutComponent},
-            { path: 'contact', component: ContactComponent },
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: '**', redirectTo: 'home', pathMatch: 'full' }
-        ]),
-        TranslateModule.forRoot()
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent},
+      { path: 'about', component: AboutComponent},
+      { path: 'contact', component: ContactComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    ]),
+    TranslateModule.forRoot({
+      provide: TranslateLoader,
+      useFactory: (createTranslateLoader),
+      deps: [Http]
+    })
   ],
   declarations: [ 
-                  AppComponent,
-                  HomeComponent,
-                  AboutComponent,
-                  ContactComponent
-                ],
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
   providers: [
-        { provide: MissingTranslationHandler, useClass: MyMissingTranslationHandler}
-    ],
-  bootstrap:    [ AppComponent ]
+    { provide: MissingTranslationHandler, useClass: MyMissingTranslationHandler}
+  ],
+  bootstrap: [ AppComponent ]
 })
 
 export class AppModule { }
